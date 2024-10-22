bd018b0c069e7cd3430b2afe5a462b1a31b35777
Themystocles <themystocles@outlook.com>
2024-10-22 09:24:42 -0300

    feat: Finalização das models

---
+++ Eventfy.Tests/Models/EventParticipantTest.cs
@@ -0,0 +1,49 @@
+﻿using Eventfy.Models;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Eventfy.Tests.Models
+{
+    public class EventParticipantTest
+    {
+        [Fact]
+        public void CreateEventParticipant_Success()
+        {
+            // Arrange
+            var event1 = new Event(1, "Palestra Pablo Marçal", "uma descrição", DateTime.Now);
+            var event2 = new Event(2, "Guanabara MySql", "Outra Descrição", DateTime.Now.AddDays(1));
+
+            var participant1 = new Participant(1, "John Doe", "john@example.com");
+            var participant2 = new Participant(2, "Jane Doe", "jane@example.com");
+
+            var eventParticipants = new List<EventParticipant>
+            {
+                new EventParticipant { IdEvent = 1, IdParticipant = 1, Event = event1, Participant = participant1 },
+                new EventParticipant { IdEvent = 2, IdParticipant = 1, Event = event2, Participant = participant1 },
+                new EventParticipant { IdEvent = 1, IdParticipant = 2, Event = event1, Participant = participant2 }
+            };
+
+            // Act
+            var eventParticipant1 = eventParticipants[0];
+            var eventParticipant2 = eventParticipants[1];
+            var eventParticipant3 = eventParticipants[2];
+
+            // Assert:
+            Assert.Equal(1, eventParticipant1.IdEvent);
+            Assert.Equal(1, eventParticipant1.IdParticipant);
+            Assert.Equal("Palestra Pablo Marçal", eventParticipant1.Event.Name);
+            Assert.Equal("John Doe", eventParticipant1.Participant.Name);
+
+            Assert.Equal(2, eventParticipant2.IdEvent);
+            Assert.Equal(1, eventParticipant2.IdParticipant);
+            Assert.Equal("Guanabara MySql", eventParticipant2.Event.Name);
+            Assert.Equal("John Doe", eventParticipant2.Participant.Name);
+
+            Assert.Equal(1, eventParticipant3.IdEvent);
+            Assert.Equal(2, eventParticipant3.IdParticipant);
+            Assert.Equal("Palestra Pablo Marçal", eventParticipant3.Event.Name);
+            Assert.Equal("Jane Doe", eventParticipant3.Participant.Name);
+        }
+    }
+}
